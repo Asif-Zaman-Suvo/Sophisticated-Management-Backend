505c79e59f79db1c4adc46ceb99f6b60269dbe44
Asif-Zaman-Suvo <asifshuvo119181@gmail.com>
2024-07-13 04:13:44 +0600

    api link

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const multer_1 = __importDefault(require("multer"));
 const path_1 = __importDefault(require("path"));
-const fs_1 = __importDefault(require("fs"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3001;
@@ -15,15 +14,10 @@ app.use((0, cors_1.default)({
     origin: "https://sophisticated-file-management-frontend.vercel.app",
     methods: "GET,POST",
 }));
-// Ensure the uploads directory exists
-const uploadDir = path_1.default.join(__dirname, "uploads");
-if (!fs_1.default.existsSync(uploadDir)) {
-    fs_1.default.mkdirSync(uploadDir);
-}
-// Set up multer for file uploads
+// gSet up multer for file uploads
 const storage = multer_1.default.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, uploadDir);
+        cb(null, "./uploads"); // Destination folder relative to the root
     },
     filename: function (req, file, cb) {
         cb(null, `${Date.now()}-${file.originalname}`);
@@ -32,8 +26,8 @@ const storage = multer_1.default.diskStorage({
 const upload = (0, multer_1.default)({ storage: storage });
 // Middleware to handle JSON responses
 app.use(express_1.default.json());
-// Serve static files from the uploads directory
-app.use("/uploads", express_1.default.static(uploadDir));
+// Serve static files from the /uploads directory
+app.use("/uploads", express_1.default.static(path_1.default.join(__dirname, "uploads")));
 // API endpoint to handle file uploads
 app.post("/api/upload", upload.single("file"), (req, res) => {
     try {
